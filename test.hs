8035af3b6cb6cc5d3be6e119d1dbda356c601856
Mikhail Puzanov <mpuzanov@ripe.net>
2015-01-08 01:27:26 +0100

    Add HUnit for testing.

--- test.hs
+++ test.hs
@@ -1,16 +1,32 @@
 import Scanner
 
 import Text.ParserCombinators.Parsec
+import Test.HUnit
 
 testParse parser string = parse parser "scanner" string
 
-main = do
+--main = do
 --  print $ testParse ident "a"
 --  print $ testParse ident "1"
 --  print $ testParse ident "a1"
 --  print $ testParse ident "a a"
-  print $ testParse stringConst ['\'', 'x', 'y', '\'']
-  print $ testParse stringConst ['\'', 'x', '\\', '\'', 'y', '\''] -- 'x\'y'
-  print $ testParse stringConst "\"xy\""
-  print $ testParse stringConst ("\"x" ++ ['\\', '"'] ++ "y\"")
+--  print $ testParse stringConst ['\'', 'x', 'y', '\'']
+--  print $ testParse stringConst ['\'', 'x', '\\', '\'', 'y', '\''] -- 'x\'y'
+--  print $ testParse stringConst "\"xy\""
+--  print $ testParse stringConst ("\"x" ++ ['\\', '"'] ++ "y\"")
+
+
+testScanner parser string token = TestCase $ assertEqual 
+  ("Should get " ++ (show token) ++ " after parsing " ++ string) 
+    token (case (testParse parser string) of 
+       Right (t, s) -> t
+    )
+
+literalTest = TestList [testScanner stringConst "\"xy\"" (SConstant "xy"),
+                        testScanner stringConst ("\"x" ++ ['\\', '"'] ++ "y\"") (SConstant "x\"y"), 
+                        testScanner stringConst ['\'', 'x', 'y', '\''] (SConstant "xy"), 
+                        testScanner stringConst ['\'', 'x', '\\', '\'', 'y', '\''] (SConstant "x'y") 
+                       ]
+
+main = runTestTT literalTest
 
